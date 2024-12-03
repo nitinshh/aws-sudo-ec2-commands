c195662110ad6e98b451891484a9506c45b9a634
Nitin Sharma <nitinsharma30302@gmail.com>
2024-12-03 12:21:03 +0530

    aws sudo ec2 commands

---
+++ commands.js
@@ -0,0 +1,44 @@
+//  aws sudo commands ================= 
+
+// all commands by one-by-one
+
+
+// sudo apt update
+// curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -   // 20 is the node version
+// sudo apt-get install -y nodejs
+// node -v and npm -v
+// npm install pm2 -g
+// git clone <repo link>
+// pm2 start app.js --name= backendApi
+// pm2 status
+
+
+// to stop/delete the pm2 process----------
+
+// pm2 stop 0
+// pm2 delete 0
+
+
+// to check the pm2 logs-----------
+
+// pm2 logs
+
+
+
+// to delete the folder---------------
+
+// sudo rm -rf foldername   
+
+
+
+// to see and kill the running ports in ubuntu-----------
+
+// sudo lsof -i -P -n
+// sudo lsof -i :<port-number>
+// sudo kill -9 <PID>
+
+
+// to change to the root directory----------
+
+// sudo -s
+

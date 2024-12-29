159c4aa5dccc4f05f3cc2d912762b9996cfd0c87
nitinshh <nitinsharma30302@gmail.com>
2024-12-29 21:00:46 +0530

    aws sudo ec2 commands

--- commands.js
+++ commands.js
@@ -3,42 +3,50 @@
 // all commands by one-by-one
 
 
-// sudo apt update
-// curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -   // 20 is the node version
-// sudo apt-get install -y nodejs
-// node -v and npm -v
-// npm install pm2 -g
-// git clone <repo link>
-// pm2 start app.js --name= backendApi
-// pm2 status
+sudo apt update
+curl -sL https://deb.nodesource.com/setup_20.x | sudo -E bash -   // 20 is the node version
+sudo apt-get install -y nodejs
+node -v and npm -v
+npm install pm2 -g
+git clone <repo link>
+pm2 start app.js --name= backendApi
+pm2 status
 
 
 // to stop/delete the pm2 process----------
 
-// pm2 stop 0
-// pm2 delete 0
+pm2 stop 0
+pm2 delete 0
 
 
 // to check the pm2 logs-----------
 
-// pm2 logs
+pm2 logs
 
 
 
 // to delete the folder---------------
 
-// sudo rm -rf foldername   
+sudo rm -rf foldername   
 
+// to change to the root directory----------
+
+sudo -s
 
 
 // to see and kill the running ports in ubuntu-----------
 
-// sudo lsof -i -P -n
-// sudo lsof -i :<port-number>
-// sudo kill -9 <PID>
+sudo lsof -i -P -n
+sudo lsof -i :<port-number>
+sudo kill -9 <PID>
 
 
-// to change to the root directory----------
 
-// sudo -s
+#!/bin/bash
+
+sudo apt update -y
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo install nodejs /usr/local/bin 
+npm install -g pm2
 
